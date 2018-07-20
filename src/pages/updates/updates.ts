545485ab60fb5ee49de7a4ab7e13926a7eba312c
fpkooij <fpkooij@outlook.com>
2018-07-20 05:40:24 -0700

    Update 20-7-2018

--- src/pages/updates/updates.ts
+++ src/pages/updates/updates.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-updates',
@@ -7,8 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class UpdatesPage {
 
-  constructor(public navCtrl: NavController) {
+  testRadioOpen: boolean;
+  testRadioResult;
 
-  }
+  constructor(public alerCtrl: AlertController) {}
+
+  ConfirmAttendee(){
+    let confirm = this.alerCtrl.create({
+      title : 'Deelnamen:',
+      message: 'Wil je toch meedoen met de bijbehorende training?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+}
+
+  // constructor(public navCtrl: NavController) {}
+  ConfirmDecline() {
+    let confirm = this.alerCtrl.create({
+      title : 'Deelnamen:',
+      message: 'Wil je toch niet meedoen met de bijbehorende training?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+}
 
 }
